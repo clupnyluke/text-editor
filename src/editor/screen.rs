7ec905834dafb5d43788b23130d4018e7e557293
Lucas Clupny <clupnyluke@gmail.com>
2024-08-14 15:41:40 -0400

    add rendering support for files larger than the terminal

--- src/editor/screen.rs
+++ src/editor/screen.rs
@@ -1,19 +1,23 @@
 use std::io::{stdout, Write};
 
-use crossterm::cursor::{position, MoveTo, MoveToColumn, RestorePosition, SavePosition};
-use crossterm::{execute, queue};
+use crossterm::cursor::{MoveTo, RestorePosition, SavePosition};
+use crossterm::queue;
 
 use super::buffer::Buffer;
 use super::terminal::Terminal;
 use super::IOResult;
 
-pub fn update_line(buffer: &Buffer, line_number: u16) -> IOResult {
-    queue!(stdout(), SavePosition, MoveTo(0, line_number))?;
+pub fn update_line(buffer: &Buffer, terminal: &Terminal, line_number: usize) -> IOResult {
+    let (term_x, _term_y) = terminal.virtual_position();
+    let (width, _height) = Terminal::size()?;
+    queue!(stdout(), SavePosition,)?;
+    Terminal::cursor_to_beginning_of_line()?;
     Terminal::clear_line_with_cursor()?;
     let default = String::new();
     if (line_number as usize) < buffer.len() {
         let line = buffer.get_line(line_number).unwrap_or(&default);
-        print!("{line}");
+        let text = &line[term_x..usize::min(term_x + width as usize, line.len())];
+        print!("{text}");
     } else {
         print!("~");
     }
@@ -22,28 +26,31 @@ pub fn update_line(buffer: &Buffer, line_number: u16) -> IOResult {
     Ok(())
 }
 
-pub fn update_current_line(buffer: &Buffer) -> IOResult {
-    update_line(buffer, position()?.1)
+pub fn update_current_line(buffer: &Buffer, terminal: &Terminal) -> IOResult {
+    update_line(buffer, terminal, Terminal::cursor_position()?.1)
 }
 
-pub fn update_line_until_eof(buffer: &Buffer, line_number: u16) -> IOResult {
-    queue!(stdout(), SavePosition, MoveTo(0, line_number),)?;
+pub fn update_line_until_eof(buffer: &Buffer, terminal: &Terminal, line_number: usize) -> IOResult {
+    let (term_x, term_y) = terminal.virtual_position();
+    let (width, height) = Terminal::size()?;
+    if line_number >= term_y && line_number <= term_y + height as usize {
+        queue!(stdout(), SavePosition)?;
+        Terminal::cursor_to(0, line_number - term_y)?;
     Terminal::clear_from_cursor_down()?;
-    let default = vec![String::new()];
-    let lines = buffer.get_lines(line_number as usize..).unwrap_or(&default);
-    let mut line_number = 0;
-    for line in lines {
-        println!("{line}");
-        queue!(stdout(), MoveToColumn(0))?;
-        line_number += 1;
+        let default = String::from("~");
+        for line_number in line_number..term_y + height {
+            let line = buffer.get_line(line_number).unwrap_or(&default);
+            let text = &line[term_x..usize::min(term_x + width as usize, line.len())];
+            if line_number == term_y + height - 1 {
+                print!("{text}");
+            } else {
+                println!("{text}");
+            }
+            Terminal::cursor_to_beginning_of_line()?;
     }
-    for _ in line_number..=Terminal::size()?.1 {
-        println!("~");
-        queue!(stdout(), MoveToColumn(0))?;
-    }
-
     queue!(stdout(), RestorePosition)?;
     stdout().flush()?;
+    }
     Ok(())
 }
 
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -50,14 +50,20 @@ impl Terminal {
         self.virtual_position
     }
 
+    pub fn cursor_position() -> Result<(usize, usize), std::io::Error> {
+        let (x, y) = position()?;
+        Ok((x as usize, y as usize))
+    }
+
     pub fn clean_up() -> IOResult {
         execute!(stdout(), EnableLineWrap)?;
         disable_raw_mode()?;
         Ok(())
     }
 
-    pub fn size() -> Result<(u16, u16), std::io::Error> {
-        size()
+    pub fn size() -> Result<(usize, usize), std::io::Error> {
+        let (x, y) = size()?;
+        Ok((x as usize, y as usize))
     }
 
     pub fn clear_all() -> IOResult {
@@ -71,7 +77,24 @@ impl Terminal {
     }
 
     pub fn clear_line_with_cursor() -> IOResult {
-        execute!(stdout(), Clear(ClearType::All))?;
+        execute!(stdout(), Clear(ClearType::CurrentLine))?;
+        Ok(())
+    }
+
+    pub fn goto_beginning_of_line(&mut self) -> IOResult {
+        execute!(stdout(), MoveToColumn(0))?;
+        self.virtual_cursor.0 = 0;
+        self.virtual_position.0 = 0;
+        Ok(())
+    }
+
+    pub fn cursor_to(x: usize, y: usize) -> IOResult {
+        execute!(stdout(), MoveTo(x as u16, y as u16))?;
+        Ok(())
+    }
+
+    pub fn cursor_to_beginning_of_line() -> IOResult {
+        execute!(stdout(), MoveToColumn(0))?;
         Ok(())
     }
 }
